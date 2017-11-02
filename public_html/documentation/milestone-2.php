109fe58d7aa79077f5a71e31022456e2ebd95cb2
Chris Owens <cowens17@cnm.edu>
2017-11-02 16:02:11 -0700

    Added milestone-2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -4,4 +4,21 @@
 		<meta charset="UTF-8">
 		<title>Content Strategy</title>
 	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<p>
+			The purpose fo my site is to attract possible employers and people looking to
+			have a website made or fixed. The way i will attract these people is by
+			showcasing my skills in web development, and provide them with enough information
+			about me so they can instantly decide whether I'm what they were looking for.
+		</p>
+		<ul>
+			<li>
+				<a href="https://bootcamp-coders.cnm.edu/~cowens17/personalwebsiteproject/public_html/documentation/images/wirefamedesktop.jpeg" target="_blank">Desktop WireFrame</a>
+			</li>
+			<li>
+				<a href="https://bootcamp-coders.cnm.edu/~cowens17/personalwebsiteproject/public_html/documentation/images/wireframephone.jpeg" target="_blank">Mobile WireFrame</a>
+			</li>
+		</ul>
+	</body>
 </html>
