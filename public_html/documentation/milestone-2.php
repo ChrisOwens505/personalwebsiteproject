37895ece934346617c410cfebfaca1223b77ff44
Chris Owens <cowens17@cnm.edu>
2017-11-02 16:41:23 -0700

    edited Content Strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,18 +7,18 @@
 	<body>
 		<h1>Content Strategy</h1>
 		<p>
-			The purpose fo my site is to attract possible employers and people looking to
-			have a website made or fixed. The way i will attract these people is by
-			showcasing my skills in web development, and provide them with enough information
-			about me so they can instantly decide whether I'm what they were looking for.
+			On my personal website, I am going to put a full description of who i am, what i do,
+			what i know, what my plans for the future are, and how I'm going to achieve them. It
+			will feature pictures of me being me, doing volunteer work, and me doing things that require
+			a lot of responsibility, so, I may look like a regular functioning adult. The site will also
+			feature my successes in web development, and showcase my abilities so employers will know
+			exactly what I'm capable of, right off the bat. I plan to have all my skills immediately visible
+			so that anyone who comes across it will know that I know what I'm doing (somewhat). Though my theme
+			may be dark, the important information will be highlighted in red so that whomever comes across my
+			site will distinctly remember the most important and beneficial skills that might be detrimental to what they
+			could use me for. Links to my resume and to my social media accounts will be present at the top of the page.
 		</p>
-		<ul>
-			<li>
-				<a href="https://bootcamp-coders.cnm.edu/~cowens17/personalwebsiteproject/public_html/documentation/images/wirefamedesktop.jpeg" target="_blank">Desktop WireFrame</a>
-			</li>
-			<li>
-				<a href="https://bootcamp-coders.cnm.edu/~cowens17/personalwebsiteproject/public_html/documentation/images/wireframephone.jpeg" target="_blank">Mobile WireFrame</a>
-			</li>
-		</ul>
+		<img src="images/wirefamedesktop.jpeg" alt="Desktop Wireframe">
+		<img src="images/wireframephone.jpeg" alt="Mobile Wireframe">
 	</body>
 </html>
