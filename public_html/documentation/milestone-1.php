4010de1505a71b3dd4ef7435bc32848f2533bd44
Chris Owens <cowens17@cnm.edu>
2017-11-07 15:19:24 -0800

    Added nav bar to project

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,48 +2,57 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 		<title> Personal Website</title>
 		<link rel="stylesheet" href="css/stylesheet.css">
 	</head>
 	<body class="text">
-		<img src="images/me.jpeg" height="10%" width="10%">
-		<h3>Christian Alexander Owens</h3>
-			<h1>Personal Website</h1>
-				<h2>Purpose</h2>
-		<p>
-			The purpose of creating this website is to make a professional portfolio focused on front-end and back-end web design
+		<nav class="navbar navbar-expand-lg navbar-light bg-dark" >
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<a class="navbar-brand text-light" href="https://bootcamp-coders.cnm.edu/~cowens17/personalwebsiteproject/public_html/documentation/milestone-1.php#!">My Personal Website</a>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+				<ul class="navbar-nav mr-auto mt-2 mt-md-0">
+					<li class="nav-item active">
+						<a class="nav-link text-light" href="christianowensresume.pdf">Resume<span class="sr-only">(current)</span></a>
+					</li>
+					<li>
+						<a class="nav-link text-light" href="https://github.com/ChrisOwens505">GitHub<span class="sr-only">(current)</span></a>
+					</li>
+					<li>
+						<a class="nav-link text-light" href="https://www.linkedin.com/in/christian-owens-3930a2143/">LinkedIn<span class="sr-only">(current)</span></a>
+					</li>
+					<li>
+						<a class="nav-link text-light" href="https://www.facebook.com/mr.mactastic">Facebook<span class="sr-only">(current)</span></a>
+					</li>
+					<li>
+						<a class="nav-link text-light" href="https://twitter.com/C0derChris">Twitter<span class="sr-only">(current)</span></a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<img class="border border-secondary" src="images/me.jpeg" height="10%" width="10%">
+		<h1 class="text-light">Christian Alexander Owens</h1>
+				<h2 class="text-light">Purpose</h2>
+		<p class="text-light">
+			The purpose of creating this website is to make a professional portfolio Showcasing my front-end and back-end web design experience
 		</p>
-		<h2>Audience</h2>
-		<p>
-			The audience of this web-page are going to be primarily professional individuals, searching for people looking into web
-			development and I.T. related positions. other people i am targeting as an audience are small business owners with a growing business,
-			company employers with a high reputation in the tech industry, and simply, my own peers.
+		<h2 class="text-light">Audience</h2>
+		<p class="text-light">
+			The audience of My web-page are going to be professional individuals, looking or someone who has experience in front end and back
+			end web design. Other people of interest are going to be people who own small businesses and are looking for someone to make or clean
+			up the website for their establishment. I hope to one day to have experience to show in web security, attack and defense.
+		</p class="text-light">
+		<h2 class="text-light">Goal</h2>
+		<p class="text-light">
+			my goal is to purchase a property, and open my own pharmacy. first I would like to open
+			a lounge specific to people who enjoy the tech industry, and use the profit from it to
+			buy what while I work towards a bio-tech degree.
 		</p>
-		<h2>Goal</h2>
-		<p>
-			The goal of this website is to give employers a way to view my professional profile,
-			and to contact me if they decide to present me with an open position. A secondary
-			goal of this website is to receive offers or recommendations to other opportunities
-			that allow me to further my education in web design and security.
-		</p>
-	<h1>Persona</h1>
-		<ul>
-			<li><strong>Name:</strong> Mac Johnson </li>
-			<li><strong>Age:</strong> 45</li>
-			<li><strong>Occupation:</strong> Founder of 4 small businesses, all related to technology. one is a store for computer parts, another
-			is a local software development company, and the last two are store that buy, sell, and trade video games and accessories.
-			</li>
-			<li><strong>Issues:</strong> long pages, small lettering, to many colors or distracting buttons, slow loading, inaccurate search results,
-			not being able to easily share a post with friends, and sloppy content placement.
-			</li>
-			<li><strong>Tech:</strong> uses a mac book pro, but still has an Samsung Galaxy s4. Plays games on his Playstation 4, but sometimes enjoys to play steam on his alienware desktop</li>
-		</ul>
-		<p>
-			Mac Johnson is a single man, living in a one room apartment, specifically for the reason of saving money. he is a lover of all things
-			technological, and devotes his life to software development. He is currently searching for a web developer that is familiar with MySql,
-			PHP, Javascript, and Java. and the position he is looking to fill has to do with overseeing the websites for 2 of his 4 small businesses.
-			he has issues with staring at a screen for long periods of time, so having a young, fresh out of college student to look over half of the
-			sites for his businesses makes managing the establishments easier on his part.
-		</p>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 	</body>
 </html>
