90dafde1b242e1be89728309b2a425178a56371c
Chris Owens <cowens17@cnm.edu>
2017-11-10 11:33:21 -0800

    added cards to pwp

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -33,24 +33,48 @@
 				</ul>
 			</div>
 		</nav>
-		<img class="border border-secondary" src="images/me.jpeg" height="10%" width="10%">
-		<h1 class="text-light">Christian Alexander Owens</h1>
-				<h2 class="text-light">Purpose</h2>
-		<p class="text-light">
-			The purpose of creating this website is to make a professional portfolio Showcasing my front-end and back-end web design experience
-		</p>
-		<h2 class="text-light">Audience</h2>
-		<p class="text-light">
-			The audience of My web-page are going to be professional individuals, looking or someone who has experience in front end and back
-			end web design. Other people of interest are going to be people who own small businesses and are looking for someone to make or clean
-			up the website for their establishment. I hope to one day to have experience to show in web security, attack and defense.
-		</p class="text-light">
-		<h2 class="text-light">Goal</h2>
-		<p class="text-light">
-			my goal is to purchase a property, and open my own pharmacy. first I would like to open
-			a lounge specific to people who enjoy the tech industry, and use the profit from it to
-			buy what while I work towards a bio-tech degree.
-		</p>
+		<div class="card-deck">
+			<div class="card text-white bg-dark mb-3">
+				<img class="card-img-top" src="images/me.jpeg" alt="Card image cap">
+				<div class="card-body">
+					<h4 class="card-title">Christian Alexander Owens</h4>
+					<p class="card-text">My name is Christian and I am Albuquerque born and raised. I have an individual passion for each subject I'm interested in, those include computer sciences, psychology, and bio-technology. I plan to attend school and achieve a degree in bio-tech, and use my knowledge of web development to somehow integrate one into the other. My goal is
+						to purchase a property, and open my own pharmacy. Me and a close friend of mine plan to start a data center company as soon as we get on the grounds to do so, this is my main goal
+						for my future.</p>
+					<p class="card-text"><small class="text-muted">Nov. 2017</small></p>
+				</div>
+			</div>
+			<div class="card text-white bg-dark mb-3">
+				<img class="card-img-top" src="images/mexico.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h4 class="card-title">Trip To Mexico</h4>
+					<p class="card-text">This is me when I went on a seven day missionary trip to mexico. We build houses for locals with unfortunate living conditions, and I personally got to practice leadership in a group with the other teenagers I was assigned to work with.</p>
+					<p class="card-text"><small class="text-muted">Feb. 2016</small></p>
+				</div>
+			</div>
+			<div class="card text-white bg-dark mb-3">
+				<img class="card-img-top" src="images/meanddad.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h4 class="card-title">Leaving Highschool</h4>
+					<p class="card-text">This is a picture of me after I decided high school was not what I was looking for, and when i recieved my high school equivalency credential.  </p>
+					<p class="card-text"><small class="text-muted">Dec. 2016</small></p>
+				</div>
+			</div>
+		</div>
+		<div class="card-deck" style="width: 32rem">
+			<div class="card text-white bg-dark mb-3">
+				<img class="card-img-top" src="images/chris.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h4 class="card-title">Working</h4>
+					<p class="card-text">Here is a picture of me teaching a small child how to safely play inside of a bubble, and also giving him the attention as if I was a fellow customer playing along.</p>
+					<p class="card-text"><small class="text-muted">Mar. 2016</small></p>
+				</div>
+			</div>
+		</div>
+		<blockquote class="blockquote">
+			<p class="mb-0">The best way to escape from your problem is to solve it.</p>
+			<footer class="blockquote-footer">Jazz <cite title="Source Title">Cartier</cite></footer>
+		</blockquote>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
