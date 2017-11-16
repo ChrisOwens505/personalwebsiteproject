8e43e8cd6a0f33c11082f0db98cd4b59e139b1cb
Chris Owens <cowens17@cnm.edu>
2017-11-16 11:44:50 -0800

    fixed all text to hac correct grammar

--- public_html/index.php
+++ public_html/index.php
@@ -4,11 +4,14 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 		<title> Personal Website</title>
 		<link rel="stylesheet" href="css/stylesheet.css">
 	</head>
 	<body class="text">
-		<nav class="navbar navbar-expand-lg navbar-light bg-dark" >
+		<nav class="navbar navbar-expand-lg navbar-light bg-dark mb-3" >
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
@@ -16,7 +19,7 @@
 			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 				<ul class="navbar-nav mr-auto mt-2 mt-md-0">
 					<li class="nav-item active">
-						<a class="nav-link text-light" href="documentation/christianowensresume.pdf">Resume<span class="sr-only">(current)</span></a>
+						<a class="nav-link text-light" href="documentation/christianowensresume.pdf">Résumé<span class="sr-only">(current)</span></a>
 					</li>
 					<li>
 						<a class="nav-link text-light" href="https://github.com/ChrisOwens505">GitHub<span class="sr-only">(current)</span></a>
@@ -56,7 +59,7 @@
 				<img class="card-img-top" src="images/meanddad.jpg" alt="Card image cap">
 				<div class="card-body">
 					<h4 class="card-title">Leaving Highschool</h4>
-					<p class="card-text">This is a picture of me after I decided high school was not what I was looking for, and when i recieved my high school equivalency credential.  </p>
+					<p class="card-text">This is a picture of me after I decided high school was not what I was looking for, so, I decided to leave and receive my high school equivalency credential. Soon after, I went to give a college a try at the age of 16. </p>
 					<p class="card-text"><small class="text-muted">Dec. 2016</small></p>
 				</div>
 			</div>
@@ -67,24 +70,30 @@
 				<img class="card-img-top" src="images/medoingwalls.jpg" alt="Card image cap">
 				<div class="card-body">
 					<h4 class="card-title">Casual work</h4>
-					<p class="card-text">Here's me doing what I usually do when my dad asks for some help. He always taught me to to have a positive and tenacious work ethic</p>
-					<p class="card-text"> <small class="text-muted">Nov. 2017</small></p>
+					<p class="card-text">Here's me doing what I usually do when my dad asks for some help. He has always taught me to to have a positive and tenacious work ethic.</p>
+					<p class="card-text"> <small class="text-muted">Sept. 2017</small></p>
 				</div>
 			</div>
 			<div class="card text-white bg-dark mb-3">
 				<img class="card-img-top" src="images/chris.jpg" alt="Card image cap">
 				<div class="card-body">
 					<h4 class="card-title">Past job</h4>
-					<p class="card-text">This one's of me when i used to work at a place called ridiculous fun. It was a bubble sport and VR game establishment, and I learned a lot about working with kids and showing people how to do simple tasks. </p>
-					<p class="card-text"><small class="text-muted">Feb. 2016</small></p>
+					<p class="card-text">This one's of me when I used to work at a place called ridiculous fun. It was a bubble sport and VR game establishment, and I learned a lot about working with kids and showing people how to do simple tasks. </p>
+					<p class="card-text"><small class="text-muted">Feb. 2017</small></p>
 				</div>
 			</div>
 			<div class="card text-white bg-dark mb-3">
 				<img class="card-img-bottom" src="images/meinmexico.jpg" >
 				<div class="card-body">
-					<h4 class="card-title">Final Note From Me</h4>
-					<p class="card-text"></p>
-					<p class="card-text"><small class="text-muted">Dec. 2016</small></p>
+					<h4 class="card-title">Contact Information</h4>
+					<p class="card-text">If you would like to reach me regarding anything, I have provided some other ways you may contact me besides through my social media.</p>
+					<ul>
+						<li>Cellular Number : 505-610-6456</li>
+						<li>Main Email Address : Chrisowens132@gmail.com</li>
+						<li>CNM Email Address : Cowens17@cnm.edu</li>
+					</ul>
+					<p>Please, if you have any questions or concerns don't hesitate to contact me as soon as possible. Thank you for visiting my personal website, youre time is greatly appreciated!</p>
+					<p class="card-text"><small class="text-muted">Last Updated Nov. 2017</small></p>
 				</div>
 			</div>
 		</div>
@@ -92,8 +101,5 @@
 			<p class="mb-0">The best way to escape from your problem is to solve it.</p>
 			<footer class="blockquote-footer">Jazz <cite title="Source Title">Cartier</cite></footer>
 		</blockquote>
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 	</body>
 </html>
