6767f310b95d3b70ab9900dc4eea8e3e4fdf62e4
Chris Owens <cowens17@cnm.edu>
2017-11-21 10:52:16 -0800

    got rid of the side bar

--- public_html/index.php
+++ public_html/index.php
@@ -36,67 +36,85 @@
 				</ul>
 			</div>
 		</nav>
-		<div class="card-deck">
-			<div class="card text-white bg-dark mb-3">
-				<img class="card-img-top" src="images/me.jpeg" alt="Card image cap">
-				<div class="card-body">
-					<h4 class="card-title">Christian Alexander Owens</h4>
-					<p class="card-text">My name is Christian, I am Albuquerque born and raised, but currently conflicted with my high                                                 aspirations to travel. I have an individual passion for each subject I'm interested in, they include web development, psychology, and bio-technology. I plan to attend school and achieve a degree in bio-tech, and use my knowledge of web development to somehow integrate one into the other. My personal goal is
-						to purchase a property, and open my own pharmacy. Me and a close friend of mine plan to start a data center company as soon as we get on the grounds to do so, this is my main goal
-						for my future.</p>
-					<p class="card-text"><small class="text-muted">Nov. 2017</small></p>
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col">
+					<div class="card-deck">
+						<div class="card text-white bg-dark mb-3">
+							<img class="card-img-top" src="images/me.jpeg" alt="Card image cap">
+							<div class="card-body">
+								<h4 class="card-title">Christian Alexander Owens</h4>
+								<p class="card-text">My name is Christian, I am Albuquerque born and raised, but currently conflicted with my high                                                 aspirations to travel. I have an individual passion for each subject I'm interested in, they include web development, psychology, and bio-technology. I plan to attend school and achieve a degree in bio-tech, and use my knowledge of web development to somehow integrate one into the other. My personal goal is
+									to purchase a property, and open my own pharmacy. Me and a close friend of mine plan to start a data center company as soon as we get on the grounds to do so, this is my main goal
+									for my future.</p>
+								<p class="card-text"><small class="text-muted">Nov. 2017</small></p>
+							</div>
+						</div>
+						<div class="card text-white bg-dark mb-3">
+							<img class="card-img-top" src="images/mexico.jpg" alt="Card image cap">
+							<div class="card-body">
+								<h4 class="card-title">Trip To Mexico</h4>
+								<p class="card-text">This is me when I went on a seven day missionary trip to mexico. We built houses for locals with unfortunate living conditions, and I personally got to practice leadership in a group of other teenagers that I was assigned to work with.</p>
+								<p class="card-text"><small class="text-muted">Feb. 2016</small></p>
+							</div>
+						</div>
+						<div class="card text-white bg-dark mb-3">
+							<img class="card-img-top" src="images/meanddad.jpg" alt="Card image cap">
+							<div class="card-body">
+								<h4 class="card-title">Leaving Highschool</h4>
+								<p class="card-text">This is a picture of me after I decided high school was not what I was looking for, so, I decided to leave and receive my high school equivalency credential. Soon after, I went to give a college a try at the age of 16. </p>
+								<p class="card-text"><small class="text-muted">Dec. 2016</small></p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="card text-white bg-dark mb-3">
-				<img class="card-img-top" src="images/mexico.jpg" alt="Card image cap">
-				<div class="card-body">
-					<h4 class="card-title">Trip To Mexico</h4>
-					<p class="card-text">This is me when I went on a seven day missionary trip to mexico. We built houses for locals with unfortunate living conditions, and I personally got to practice leadership in a group of other teenagers that I was assigned to work with.</p>
-					<p class="card-text"><small class="text-muted">Feb. 2016</small></p>
-				</div>
-			</div>
-			<div class="card text-white bg-dark mb-3">
-				<img class="card-img-top" src="images/meanddad.jpg" alt="Card image cap">
-				<div class="card-body">
-					<h4 class="card-title">Leaving Highschool</h4>
-					<p class="card-text">This is a picture of me after I decided high school was not what I was looking for, so, I decided to leave and receive my high school equivalency credential. Soon after, I went to give a college a try at the age of 16. </p>
-					<p class="card-text"><small class="text-muted">Dec. 2016</small></p>
-				</div>
-			</div>
-		</div>
-		<p>If you have a passion, work hard and make it happen. - Unknown</p>
-		<div class="card-deck">
-			<div class="card text-white bg-dark mb-3">
-				<img class="card-img-top" src="images/medoingwalls.jpg" alt="Card image cap">
-				<div class="card-body">
-					<h4 class="card-title">Casual work</h4>
-					<p class="card-text">Here's me doing what I usually do when my dad asks for some help. He has always taught me to to have a positive and tenacious work ethic.</p>
-					<p class="card-text"> <small class="text-muted">Sept. 2017</small></p>
-				</div>
-			</div>
-			<div class="card text-white bg-dark mb-3">
-				<img class="card-img-top" src="images/chris.jpg" alt="Card image cap">
-				<div class="card-body">
-					<h4 class="card-title">Past job</h4>
-					<p class="card-text">This one's of me when I used to work at a place called ridiculous fun. It was a bubble sport and VR game establishment, and I learned a lot about working with kids and showing people how to do simple tasks. </p>
-					<p class="card-text"><small class="text-muted">Feb. 2017</small></p>
-				</div>
-			</div>
-			<div class="card text-white bg-dark mb-3">
-				<img class="card-img-bottom" src="images/meinmexico.jpg" >
-				<div class="card-body">
-					<h4 class="card-title">Contact Information</h4>
-					<p class="card-text">If you would like to reach me regarding anything, I have provided some other ways you may contact me besides through my social media.</p>
-					<ul>
-						<li>Cellular Number : 505-610-6456</li>
-						<li>Main Email Address : Chrisowens132@gmail.com</li>
-						<li>CNM Email Address : Cowens17@cnm.edu</li>
-					</ul>
-					<p>Please, if you have any questions or concerns don't hesitate to contact me as soon as possible. Thank you for visiting my personal website, youre time is greatly appreciated!</p>
-					<p class="card-text"><small class="text-muted">Last Updated Nov. 2017</small></p>
+			<div class="row">
+				<div class="col">
+					<p>If you have a passion, work hard and make it happen. - Unknown</p>
 				</div>
 			</div>
 		</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col">
+			<div class="card-deck">
+				<div class="card text-white bg-dark mb-3">
+					<img class="card-img-top" src="images/medoingwalls.jpg" alt="Card image cap">
+					<div class="card-body">
+						<h4 class="card-title">Casual work</h4>
+						<p class="card-text">Here's me doing what I usually do when my dad asks for some help. He has always taught me to to have a positive and tenacious work ethic.</p>
+						<p class="card-text"> <small class="text-muted">Sept. 2017</small></p>
+					</div>
+				</div>
+				<div class="card text-white bg-dark mb-3">
+					<img class="card-img-top" src="images/chris.jpg" alt="Card image cap">
+					<div class="card-body">
+						<h4 class="card-title">Past Employment</h4>
+						<p class="card-text">This one's of me when I used to work at a place called ridiculous fun. It was a bubble sport and VR game establishment, and I learned a lot about working with kids and showing people how to do simple tasks. </p>
+						<p class="card-text"><small class="text-muted">Feb. 2017</small></p>
+					</div>
+				</div>
+				<div class="card text-white bg-dark mb-3">
+					<img class="card-img-bottom" src="images/meinmexico.jpg" >
+					<div class="card-body">
+						<h4 class="card-title">Contact Information</h4>
+						<p class="card-text">If you would like to reach me regarding anything, I have provided some other ways you may contact me besides through my social media.</p>
+						<ul>
+							<li>Cellular Number : 505-610-6456</li>
+							<li>Main Email Address : Chrisowens132@gmail.com</li>
+							<li>CNM Email Address : Cowens17@cnm.edu</li>
+						</ul>
+						<p>Please, if you have any questions or concerns, don't hesitate to contact me as soon as possible. Thank you for visiting my personal website, you're time is greatly appreciated!</p>
+						<p class="card-text"><small class="text-muted">Last Updated Nov. 2017</small></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 		<blockquote class="blockquote">
 			<p class="mb-0">The best way to escape from your problem is to solve it.</p>
 			<footer class="blockquote-footer">Jazz <cite title="Source Title">Cartier</cite></footer>
