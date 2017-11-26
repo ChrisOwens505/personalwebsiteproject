7669ab3b418b55cf2fe423456c32ff1d737456f3
Chris Owens <cowens17@cnm.edu>
2017-11-26 12:19:09 -0800

    turned link names into icons

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
@@ -15,23 +16,23 @@
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
-			<a class="navbar-brand text-light" href="https://bootcamp-coders.cnm.edu/~cowens17/personalwebsiteproject/public_html/documentation/milestone-1.php#!">My Personal Website</a>
+			<a class="navbar-brand text-success" href="https://bootcamp-coders.cnm.edu/~cowens17/personalwebsiteproject/public_html/documentation/milestone-1.php#!">My Personal Website</a>
 			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 				<ul class="navbar-nav mr-auto mt-2 mt-md-0">
 					<li class="nav-item active">
-						<a class="nav-link text-light" href="documentation/christianowensresume.pdf">Résumé<span class="sr-only">(current)</span></a>
+						<a class="nav-link text-success" href="documentation/christianowensresume.pdf">Résumé<span class="sr-only">(current)</span></a>
 					</li>
 					<li>
-						<a class="nav-link text-light" href="https://github.com/ChrisOwens505">GitHub<span class="sr-only">(current)</span></a>
+						<a class="nav-link fa fa-github text-success" href="https://github.com/ChrisOwens505" target="_blank"><span class="sr-only">(current)</span></a>
 					</li>
 					<li>
-						<a class="nav-link text-light" href="https://www.linkedin.com/in/christian-owens-3930a2143/">LinkedIn<span class="sr-only">(current)</span></a>
+						<a class="nav-link fa fa-linkedin text-success" href="https://www.linkedin.com/in/christian-owens-3930a2143/" target="_blank"><span class="sr-only">(current)</span></a>
 					</li>
 					<li>
-						<a class="nav-link text-light" href="https://www.facebook.com/mr.mactastic">Facebook<span class="sr-only">(current)</span></a>
+						<a class="nav-link fa fa-facebook text-success" href="https://www.facebook.com/mr.mactastic" target="_blank"><span class="sr-only">(current)</span></a>
 					</li>
 					<li>
-						<a class="nav-link text-light" href="https://twitter.com/C0derChris">Twitter<span class="sr-only">(current)</span></a>
+						<a class="nav-link fa fa-twitter text-success" href="https://twitter.com/C0derChris" target="_blank"><span class="sr-only">(current)</span></a>
 					</li>
 				</ul>
 			</div>
@@ -41,7 +42,7 @@
 			<div class="row">
 				<div class="col">
 					<div class="card-deck">
-						<div class="card text-white bg-dark mb-3">
+						<div class="card text-success bg-dark mb-3">
 							<img class="card-img-top" src="images/me.jpeg" alt="Card image cap">
 							<div class="card-body">
 								<h4 class="card-title">Christian Alexander Owens</h4>
@@ -51,16 +52,16 @@
 								<p class="card-text"><small class="text-muted">Nov. 2017</small></p>
 							</div>
 						</div>
-						<div class="card text-white bg-dark mb-3">
-							<img class="card-img-top" src="images/mexico.jpg" alt="Card image cap">
+						<div class="card text-success bg-dark mb-3">
+							<img class="card-img-top" src="images/meandelchapo.jpeg" alt="Card image cap">
 							<div class="card-body">
 								<h4 class="card-title">Trip To Mexico</h4>
 								<p class="card-text">This is me when I went on a seven day missionary trip to mexico. We built houses for locals with unfortunate living conditions, and I personally got to practice leadership in a group of other teenagers that I was assigned to work with.</p>
 								<p class="card-text"><small class="text-muted">Feb. 2016</small></p>
 							</div>
 						</div>
-						<div class="card text-white bg-dark mb-3">
-							<img class="card-img-top" src="images/meanddad.jpg" alt="Card image cap">
+						<div class="card text-success bg-dark mb-3">
+							<img class="card-img-top" src="images/meandmydad.jpeg" alt="Card image cap">
 							<div class="card-body">
 								<h4 class="card-title">Leaving Highschool</h4>
 								<p class="card-text">This is a picture of me after I decided high school was not what I was looking for, so, I decided to leave and receive my high school equivalency credential. Soon after, I went to give a college a try at the age of 16. </p>
@@ -70,44 +71,54 @@
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col">
-					<p>If you have a passion, work hard and make it happen. - Unknown</p>
-				</div>
-			</div>
-		</div>
+
 <div class="container-fluid">
 	<div class="row">
 		<div class="col">
 			<div class="card-deck">
-				<div class="card text-white bg-dark mb-3">
-					<img class="card-img-top" src="images/medoingwalls.jpg" alt="Card image cap">
+				<div class="card text-success bg-dark mb-3">
+					<img class="card-img-top" src="images/itsjustdrywall.jpeg" alt="Card image cap">
 					<div class="card-body">
 						<h4 class="card-title">Casual work</h4>
 						<p class="card-text">Here's me doing what I usually do when my dad asks for some help. He has always taught me to to have a positive and tenacious work ethic.</p>
 						<p class="card-text"> <small class="text-muted">Sept. 2017</small></p>
 					</div>
 				</div>
-				<div class="card text-white bg-dark mb-3">
-					<img class="card-img-top" src="images/chris.jpg" alt="Card image cap">
+				<div class="card text-success bg-dark mb-3">
+					<img class="card-img-top" src="images/pastwork.jpeg">
 					<div class="card-body">
 						<h4 class="card-title">Past Employment</h4>
 						<p class="card-text">This one's of me when I used to work at a place called ridiculous fun. It was a bubble sport and VR game establishment, and I learned a lot about working with kids and showing people how to do simple tasks. </p>
 						<p class="card-text"><small class="text-muted">Feb. 2017</small></p>
 					</div>
 				</div>
-				<div class="card text-white bg-dark mb-3">
-					<img class="card-img-bottom" src="images/meinmexico.jpg" >
+				<div class="card text-success bg-dark mb-3">
+					<img class="card-img-bottom" src="images/meinpaint.jpeg" >
 					<div class="card-body">
-						<h4 class="card-title">Contact Information</h4>
-						<p class="card-text">If you would like to reach me regarding anything, I have provided some other ways you may contact me besides through my social media.</p>
-						<ul>
-							<li>Cellular Number : 505-610-6456</li>
-							<li>Main Email Address : Chrisowens132@gmail.com</li>
-							<li>CNM Email Address : Cowens17@cnm.edu</li>
-						</ul>
-						<p>Please, if you have any questions or concerns, don't hesitate to contact me as soon as possible. Thank you for visiting my personal website, you're time is greatly appreciated!</p>
-						<p class="card-text"><small class="text-muted">Last Updated Nov. 2017</small></p>
+						<p>
+							If you have any questions regarding employment of just want to leave me a message, feel free to contact me!
+						</p>
+						<div class="container">
+							<form id="contact" action="" method="post">
+								<h3>Go Ahead!</h3>
+								<h4>I will respond as soon as I get the chance!</h4>
+								<fieldset>
+									<input placeholder="Name?" type="text" tabindex="1" required autofocus>
+								</fieldset>
+								<fieldset>
+									<input placeholder="Email?" type="email" tabindex="2" required>
+								</fieldset>
+								<fieldset>
+									<input placeholder="Cell Number?" type="tel" tabindex="3" required>
+								</fieldset>
+								<fieldset>
+									<textarea placeholder="Leave a Message" tabindex="5" required></textarea>
+								</fieldset>
+								<fieldset>
+									<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+								</fieldset>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -116,7 +127,7 @@
 </div>
 
 		<blockquote class="blockquote">
-			<p class="mb-0">The best way to escape from your problem is to solve it.</p>
+			<p class="mb-0 text-white">The best way to escape from your problem is to solve it.</p>
 			<footer class="blockquote-footer">Jazz <cite title="Source Title">Cartier</cite></footer>
 		</blockquote>
 	</body>
