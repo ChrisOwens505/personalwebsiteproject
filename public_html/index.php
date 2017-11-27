a99ba870a46510766f5fe53d772465e45cdac0bc
Chris Owens <cowens17@cnm.edu>
2017-11-27 14:06:17 -0800

    made images have green border

--- public_html/index.php
+++ public_html/index.php
@@ -18,7 +18,7 @@
 	</head>
 	<body class="text">
 
-		<nav class="navbar navbar-expand-lg navbar-light bg-dark mb-3" >
+		<nav class="navbar navbar-expand-lg navbar-light bg-dark mb-3 border border-success" >
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 
 				<span class="navbar-toggler-icon"></span>
@@ -27,9 +27,6 @@
 			<a class="navbar-brand text-white" href="https://bootcamp-coders.cnm.edu/~cowens17/personalwebsiteproject/public_html/documentation/milestone-1.php#!">C.O</a>
 			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 				<ul class="navbar-nav mr-auto mt-2 mt-md-0">
-					<li class="nav-item active">
-						<a class="nav-link text-white" href="documentation/christianowensresume.pdf">Résumé<span class="sr-only">(current)</span></a>
-					</li>
 					<li>
 						<a class="nav-link fa fa-github text-white" href="https://github.com/ChrisOwens505" target="_blank"><span class="sr-only">(current)</span></a>
 					</li>
@@ -42,37 +39,45 @@
 					<li>
 						<a class="nav-link fa fa-twitter text-white" href="https://twitter.com/C0derChris" target="_blank"><span class="sr-only">(current)</span></a>
 					</li>
+					<li class="nav-item active">
+						<a class="nav-link text-white" href="documentation/christianowensresume.pdf">Résumé<span class="sr-only">(current)</span></a>
+					</li>
 				</ul>
 			</div>
 		</nav>
-		<div class="parallax-container">
-			<div class="parallax"><img src="images/programmar.jpeg"></div>
+		<div class="media">
+			<img class="mr-3" src="images/awatts.jpg" alt="Alan Watts">
+			<div class="media-body">
+				<h5 class="mt-0">The Dream Of Life.</h5>
+				<p>
+					If you awaken from this illusion and you understand that black implies white, self implies other, life implies death (or shall I say death implies life?), you can feel yourself – not as a stranger in the world, not as something here on probation, not as something that has arrived here by fluke - but you can begin to feel your own existence as absolutely fundamental.
+				</p>
+				<p>~ Alan Watts</p>
+			</div>
 		</div>
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col">
 					<div class="card-deck">
 						<div class="card text-white bg-dark mb-3">
-							<img class="card-img-top" src="images/me.jpeg" alt="Card image cap">
-							<div class="card-body">
+							<img class="card-img-top border border-success" src="images/me.jpeg" alt="Card image cap">
+							<div class="card-body border border-success">
 								<h4 class="card-title">Christian Alexander Owens</h4>
-								<p class="card-text">My name is Christian, I am Albuquerque born and raised, but currently conflicted with my high                                                 aspirations to travel. I have an individual passion for each subject I'm interested in, they include web development, psychology, and bio-technology. I plan to attend school and achieve a degree in bio-tech, and use my knowledge of web development to somehow integrate one into the other. My personal goal is
-									to purchase a property, and open my own pharmacy. Me and a close friend of mine plan to start a data center company as soon as we get on the grounds to do so, this is my main goal
-									for my future.</p>
+								<p class="card-text">My name is Christian, I am Albuquerque born and raised, but continuously distressed by my tenacious                                                 aspirations to travel. I have independent passions for each subject I enjoy to invest my time into, they include web development, minor psychology, and bio-technology( bio-medicine). I plan to soon attend classes at CNM to achieve a bachelors degree in bio-medicine, while at the same time, using my knowledge of web development to create a comfortable starting point for myself. My personal goal is to purchase a property, and open my own pharmacy. Me and a close friend of mine plan to start a data center company as soon as we get on the grounds to do so, this is the main goal for my future.</p>
 								<p class="card-text"><small class="text-muted">Nov. 2017</small></p>
 							</div>
 						</div>
 						<div class="card text-white bg-dark mb-3">
-							<img class="card-img-top" src="images/meandelchapo.jpeg" alt="Card image cap">
-							<div class="card-body">
+							<img class="card-img-top border border-success" src="images/meandelchapo.jpeg" alt="Card image cap">
+							<div class="card-body border border-success">
 								<h4 class="card-title">Trip To Mexico</h4>
 								<p class="card-text">This is me when I went on a seven day missionary trip to mexico. We built houses for locals with unfortunate living conditions, and I personally got to practice leadership in a group of other teenagers that I was assigned to work with.</p>
 								<p class="card-text"><small class="text-muted">Feb. 2016</small></p>
 							</div>
 						</div>
 						<div class="card text-white bg-dark mb-3">
-							<img class="card-img-top" src="images/meandmydad.jpeg" alt="Card image cap">
-							<div class="card-body">
+							<img class="card-img-top border border-success" src="images/meandmydad.jpeg" alt="Card image cap">
+							<div class="card-body border border-success">
 								<h4 class="card-title">Leaving Highschool</h4>
 								<p class="card-text">This is a picture of me after I decided high school was not what I was looking for, so, I decided to leave and receive my high school equivalency credential. Soon after, I went to give a college a try at the age of 16. </p>
 								<p class="card-text"><small class="text-muted">Dec. 2016</small></p>
@@ -87,31 +92,31 @@
 		<div class="col">
 			<div class="card-deck">
 				<div class="card text-white bg-dark mb-3">
-					<img class="card-img-top" src="images/itsjustdrywall.jpeg" alt="Card image cap">
-					<div class="card-body">
+					<img class="card-img-top border border-success" src="images/itsjustdrywall.jpeg" alt="Card image cap">
+					<div class="card-body border border-success">
 						<h4 class="card-title">Casual work</h4>
 						<p class="card-text">Here's me doing what I usually do when my dad asks for some help. He has always taught me to to have a positive and tenacious work ethic.</p>
 						<p class="card-text"> <small class="text-muted">Sept. 2017</small></p>
 					</div>
 				</div>
 				<div class="card text-white bg-dark mb-3">
-					<img class="card-img-top" src="images/pastwork.jpeg">
-					<div class="card-body">
+					<img class="card-img-top border border-success" src="images/pastwork.jpeg">
+					<div class="card-body border border-success">
 						<h4 class="card-title">Past Employment</h4>
 						<p class="card-text">This one's of me when I used to work at a place called ridiculous fun. It was a bubble sport and VR game establishment, and I learned a lot about working with kids and showing people how to do simple tasks. </p>
 						<p class="card-text"><small class="text-muted">Feb. 2017</small></p>
 					</div>
 				</div>
 				<div class="card text-white bg-dark mb-3">
-					<img class="card-img-bottom" src="images/meinpaint.jpeg" >
-					<div class="card-body">
+					<img class="card-img-bottom border border-success" src="images/meinpaint.jpeg" >
+					<div class="card-body border border-success">
 						<p>
-							If you have any questions regarding employment of just want to leave me a message, feel free to contact me!
+							If you have any questions regarding employment, interests, or you may just want to leave me a message, feel free to contact me!
 						</p>
 						<div class="container">
 							<form id="contact" action="" method="post">
 								<h3>Go Ahead!</h3>
-								<h4>I will respond as soon as I get the chance!</h4>
+								<h4>I will respond to your message very soon.</h4>
 								<fieldset class="mind">
 									<input placeholder="Name?" type="text" tabindex="1" required autofocus>
 								</fieldset>
@@ -119,13 +124,13 @@
 									<input placeholder="Email?" type="email" tabindex="2" required>
 								</fieldset>
 								<fieldset class="mind">
-									<input placeholder="Cell Number?" type="tel" tabindex="3" required>
+									<input placeholder="Cell?" type="tel" tabindex="3" required>
 								</fieldset>
 								<fieldset class="mind">
 									<textarea placeholder="Leave a Message" tabindex="5" required></textarea>
 								</fieldset>
 								<fieldset>
-									<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+									<button name="submit" type="submit" id="contact-submit" class="submit" data-submit="...Sending">Submit</button>
 								</fieldset>
 							</form>
 						</div>
@@ -137,8 +142,8 @@
 </div>
 
 		<blockquote class="blockquote">
-			<p class="mb-0 text-white">The best way to escape from your problem is to solve it.</p>
-			<footer class="blockquote-footer">Jazz <cite title="Source Title">Cartier</cite></footer>
+			<p class="mb-0 text-white">The only way to make sense out of change is to plunge into it, move with it, and join the dance.</p>
+			<footer class="blockquote-footer">Alan<cite title="Source Title"> Watts</cite></footer>
 		</blockquote>
 	</body>
 </html>
