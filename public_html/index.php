be335b6485e2660c68414347aa6804bc282eb2d6
Chris Owens <cowens17@cnm.edu>
2017-12-06 10:03:14 -0800

    cleaned my code up making it easier to read

--- public_html/index.php
+++ public_html/index.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		/** Meta Tags
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		/**
-		* CSS/bootstrap links
+		* CSS/bootstrap
 		*/
 		<link rel="stylesheet" href="css/stylesheet.css">
 		<link rel="stylesheet" href="font-awesome-animation.min.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" crossorigin="anonymous"></script>
 		/**
-		* JavaScript Links
+		* JavaScript
 		*/
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
@@ -25,14 +26,13 @@
 	</head>
 
 	<body class="text">
+
 		<nav class="navbar navbar-expand-lg navbar-light mb-3 " >
 			<button class="navbar-toggler navbar-toggler-right bg-white" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<a class="navbar-brand text-white" href="https://bootcamp-coders.cnm.edu/~cowens17/personalwebsiteproject/public_html/">C.O</a>
 			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-
 				<ul class="navbar-nav mr-auto mt-2 mt-md-0">
 					<li>
 						<a class="nav-link fa fa-github text-white" href="https://github.com/ChrisOwens505" target="_blank"><span class="sr-only">(current)</span></a>
@@ -58,11 +58,14 @@
 			<div class="media-body">
 				<h5 class="mt-0">The Dream Of Life.</h5>
 				<p>
-					If you awaken from this illusion and you understand that black implies white, self implies other, life implies death (or shall I say death implies life?), you can feel yourself – not as a stranger in the world, not as something here on probation, not as something that has arrived here by fluke - but you can begin to feel your own existence as absolutely fundamental.
+					If you awaken from this illusion and you understand that black implies white, self implies other, life implies death (or shall
+					I say death implies life?), you can feel yourself – not as a stranger in the world, not as something here on probation, not as
+					something that has arrived here by fluke - but you can begin to feel your own existence as absolutely fundamental.
 				</p>
 				<p>~ Alan Watts</p>
 			</div>
 		</div>
+
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col">
@@ -71,7 +74,13 @@
 							<img class="card-img-top border border-white" src="images/me.jpeg" alt="Card image cap">
 							<div class="card-body border border-white">
 								<h4 class="card-title">Christian Alexander Owens</h4>
-								<p class="card-text">My name is Christian, I am Albuquerque born and raised, but continuously distressed by my tenacious                                                 aspirations to travel. I have independent passions for each subject I enjoy to invest my time into, they include web development, minor psychology, and bio-technology (bio-medicine). I plan to soon attend classes at CNM to achieve a bachelors degree in bio-medicine, while at the same time, using my knowledge of web development to create a comfortable starting point for myself. My personal goal is to purchase a property, and open my own pharmacy. Unfortunately it is a goal I see farther along in my future, So, Me and a close friend of mine plan to start a data center company as soon as we get on the grounds to do so, this is the main goal for my future.</p>
+								<p class="card-text">My name is Christian, I am Albuquerque born and raised, but continuously distressed by my
+									tenacious aspirations to travel. I have independent passions for each subject I enjoy to invest my time into, they
+									include web development, minor psychology, and bio-technology (bio-medicine). I plan to soon attend classes at CNM
+									to achieve a bachelors degree in bio-medicine, while at the same time, using my knowledge of web development to
+									create a comfortable starting point for myself. My personal goal is to purchase a property, and open my own
+									pharmacy. Unfortunately it is a goal I see farther along in my future, So, Me and a close friend of mine plan to
+									start a data center company as soon as we get on the grounds to do so, this is the main goal for my future.</p>
 								<p class="card-text"><small class="text-muted">Nov. 2017</small></p>
 							</div>
 						</div>
@@ -79,7 +88,10 @@
 							<img class="card-img-top border border-white" src="images/meanddylan.jpg" alt="Card image cap">
 							<div class="card-body border border-white">
 								<h4 class="card-title">Programming!</h4>
-								<p class="card-text">I could not have asked to be in a better programming course than I had the privilege to attend. I've been shown the ropes of full-stack web development, so I am familiar with languages such as HTML, CSS, PHP, Angular, MySQL, JQuery, and JavaScript. We were also taught the in's and out's of Object Oriented Programming, but, the most valuable skill I took from the boot-camp was the skill to learn how to learn. #DeepDiveCoding </p>
+								<p class="card-text">I could not have asked to be in a better programming course than I had the privilege to attend.
+									I've been shown the ropes of full-stack web development, so I am familiar with languages such as HTML, CSS, PHP,
+									Angular, MySQL, JQuery, and JavaScript. We were also taught the in's and out's of Object Oriented Programming,
+									but, the most valuable skill I took from the boot-camp was the skill to learn how to learn. #DeepDiveCoding </p>
 								<p class="card-text"><small class="text-muted">Oct. 2017</small></p>
 							</div>
 						</div>
@@ -87,7 +99,8 @@
 							<img class="card-img-top border border-white" src="images/meandmatt.jpg" alt="Card image cap">
 							<div class="card-body border border-white">
 									<h4>I Love Programming!</h4>
-									<p>I am looking for any opportunity that may be available to me. As long as I can get my hands onto some code, I'll never lose the passion i have for this profession. </p>
+									<p>I am looking for any opportunity that may be available to me. As long as I can get my hands onto some code,
+										I'll never lose the passion i have for this profession. </p>
 									<h3>Feel Free To Contact Me!</h3>
 									<h4>I will respond to your message very soon.</h4>
 									<form id="contact-form" action="php/mailer.php" method="post" novalidate>
